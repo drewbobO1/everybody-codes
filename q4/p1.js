f659bd0e74fc33334074c884ea8f0602cb1743ca
Drew Giambattista <drew.giambattista@aviture.us.com>
2026-02-05 10:26:56 -0600

    feat: Q4 P1 accepted solution--partial Q3 P1 solution (close but not there)

---
+++ q4/p1.js
@@ -0,0 +1,49 @@
+const fs = require("node:fs");
+
+function getRawInput() {
+  const dataAsString = fs.readFileSync("p1-input.txt").toString();
+  return dataAsString;
+}
+
+const rawInput = getRawInput();
+
+// Part 1
+// Forgot to convert strings to nums...
+// Otherwise there were only a couple minor errors and I got it fairly quick
+
+const nailHeightsStrs = rawInput.split("\n");
+const nailHeightsNums = strArrToNumArr(nailHeightsStrs);
+
+const lowestNail = getLowestNum(nailHeightsNums);
+const requiredStrikes = countTotalDif(nailHeightsNums, lowestNail);
+
+return requiredStrikes;
+
+function getLowestNum(nails) {
+  let lowestNum = nails[0];
+  for (let nail of nails) {
+    if (nail < lowestNum) {
+      lowestNum = nail;
+    }
+  }
+  return lowestNum;
+}
+
+function countTotalDif(nails, lowestNum) {
+  let numOfStrikes = 0;
+  for (let nail of nails) {
+    while (nail > lowestNum) {
+      nail -= 1;
+      numOfStrikes += 1;
+    }
+  }
+  return numOfStrikes;
+}
+
+function strArrToNumArr(strArr) {
+  let numArr = [];
+  strArr.forEach((_, i) => {
+    numArr.push(Number(strArr[i]));
+  });
+  return numArr;
+}
